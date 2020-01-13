ebe6715a4425ab8835e490fad4f6522ed44d056b
KaosEngineer <Deep.exe5>
2020-01-13 19:09:35 +0300

    Created new examples folder for structured uncertainty evaluation. Added new file called uncertainty.py which impelements ensemble-based measures of uncertainty. Adds new class SequenceScorerWithUncertainty to sequence_scorer. Adds a new generator obtain --compute-uncertainty.
    Added new measures of uncertainty based on AEP estimate of entropy of stochastic process...

--- fairseq/uncertainty.py
+++ fairseq/uncertainty.py
@@ -60,10 +60,11 @@ def token_uncertainties(probs, epsilon=1e-12):
 
 
 def aep_uncertainty(probs, tgt_len, epsilon=1e-14):
-    mean_prob = torch.mean(torch.logsumexp(probs, dim=1))
+    log_probs = torch.log(probs)
+
+    mean_prob = torch.mean(torch.exp(torch.sum(log_probs, dim=1)))
     total_unc = -torch.log(mean_prob) / tgt_len
 
-    log_probs = torch.log(probs)
     data_unc = -torch.mean(log_probs)
 
     mi = total_unc - data_unc
