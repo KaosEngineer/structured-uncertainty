baf09bfc093d3329046975c2b721a0a92b07a308
Maksim Ryabinin <mryab@yandex-team.ru>
2020-03-09 23:10:56 +0300

    Add initial version of distillation task/criterion

---
+++ fairseq/criterions/distillation.py
@@ -0,0 +1,123 @@
+import torch
+from torch.nn import functional as F
+
+from fairseq import utils
+from . import FairseqCriterion, register_criterion
+
+
+class DirichletEnDDLoss(object):
+    """Standard Negative Log-likelihood of the ensemble predictions"""
+
+    def __init__(self, smoothing=1e-8, teacher_prob_smoothing=1e-3):
+        self.smooth_val = smoothing
+        self.tp_scaling = 1 - teacher_prob_smoothing
+
+    def __call__(self, *args):
+        return self.forward(*args)
+
+    def forward(self, logits, teacher_logits, temp=1.0):
+        alphas = torch.exp(logits / temp)
+        precision = torch.sum(alphas, dim=1)
+
+        teacher_probs = F.softmax(teacher_logits / temp, dim=2)
+        # Smooth for num. stability:
+        probs_mean = 1 / (teacher_probs.size()[2])
+        # Subtract mean, scale down, add mean back)
+        teacher_probs = self.tp_scaling * (teacher_probs - probs_mean) + probs_mean
+        assert torch.all(teacher_probs != 0).item()
+
+        log_teacher_probs_geo_mean = torch.mean(torch.log(teacher_probs + self.smooth_val), dim=1)
+        assert torch.all(torch.isfinite(log_teacher_probs_geo_mean)).item()
+
+        # Define the cost in two parts (dependent on targets and independent of targets)
+        target_independent_term = torch.sum(torch.lgamma(alphas + self.smooth_val), dim=1) \
+                                  - torch.lgamma(precision + self.smooth_val)
+        assert torch.all(torch.isfinite(target_independent_term)).item()
+
+        target_dependent_term = - torch.sum((alphas - 1.) * log_teacher_probs_geo_mean, dim=1)
+        assert torch.all(torch.isfinite(target_dependent_term)).item()
+
+        cost = target_dependent_term + target_independent_term
+        assert torch.all(torch.isfinite(cost)).item()
+
+        return torch.mean(cost) * (temp ** 2)
+
+
+@register_criterion('sequence_distribution_distillation')
+class SequenceDistributionDistillationCritertion(FairseqCriterion):
+
+    def __init__(self, args, task):
+        super().__init__(args, task)
+        self.smooth_val = 1e-8
+        self.tp_scaling = 1 - 1e-3
+
+    def forward(self, model, sample, reduce=True):
+        # batch x len x mixture_size x n_tokens
+        net_output = model(**sample['net_input'])
+
+        # batch x len x ensemble_size x n_tokens
+        ensemble_logits = sample['ensemble_logits']
+
+        loss = self.compute_loss(net_output, ensemble_logits, sample, reduce=reduce, temp=self.temp)
+
+        sample_size = sample['target'].size(0) if self.args.sentence_avg else sample['ntokens']
+        logging_output = {
+            'loss': utils.item(loss.data) if reduce else loss.data,
+            'ntokens': sample['ntokens'],
+            'nsentences': sample['target'].size(0),
+            'sample_size': sample_size,
+        }
+        return loss, sample_size, logging_output
+
+    def compute_loss(self, net_output, ensemble_logits, sample, reduce, temp=1.0):
+        # TODO add support for mixtures
+        # TODO somehow we need to anneal temperature
+
+        logits = net_output[0]
+
+        alphas = torch.exp(logits / temp)
+        precision = torch.sum(alphas, dim=-1)
+
+        teacher_probs = F.softmax(ensemble_logits / temp, dim=-1)
+        # Smooth for num. stability:
+        probs_mean = 1 / teacher_probs.size(-1)
+        # Subtract mean, scale down, add mean back)
+        teacher_probs = self.tp_scaling * (teacher_probs - probs_mean) + probs_mean
+        assert torch.all(teacher_probs != 0).item()
+
+        log_teacher_probs_geo_mean = torch.mean(torch.log(teacher_probs + self.smooth_val), dim=-2)
+        assert torch.all(torch.isfinite(log_teacher_probs_geo_mean)).item()
+
+        # Define the cost in two parts (dependent on targets and independent of targets)
+        target_independent_term = torch.sum(torch.lgamma(alphas + self.smooth_val), dim=-2) \
+                                  - torch.lgamma(precision + self.smooth_val)
+        assert torch.all(torch.isfinite(target_independent_term)).item()
+
+        target_dependent_term = - torch.sum((alphas - 1.) * log_teacher_probs_geo_mean, dim=-2)
+        assert torch.all(torch.isfinite(target_dependent_term)).item()
+
+        cost = target_dependent_term + target_independent_term
+        assert torch.all(torch.isfinite(cost)).item()
+
+        cost *= temp ** 2
+
+        # mask loss for padding tokens
+        pad_mask = sample['target'].eq(self.padding_idx)
+        cost.masked_fill_(pad_mask, 0.)
+
+        if reduce:
+            return torch.sum(cost)
+        return cost
+
+    @staticmethod
+    def aggregate_logging_outputs(logging_outputs):
+        """Aggregate logging outputs from data parallel training."""
+        ntokens = sum(log.get('ntokens', 0) for log in logging_outputs)
+        nsentences = sum(log.get('nsentences', 0) for log in logging_outputs)
+        sample_size = sum(log.get('sample_size', 0) for log in logging_outputs)
+        return {
+            'loss': sum(log.get('loss', 0) for log in logging_outputs) / sample_size if sample_size > 0 else 0.,
+            'ntokens': ntokens,
+            'nsentences': nsentences,
+            'sample_size': sample_size,
+        }
---
+++ fairseq/tasks/distillation.py
@@ -0,0 +1,132 @@
+import os
+
+import torch
+
+from fairseq import options, checkpoint_utils
+from fairseq.data import (
+    data_utils,
+)
+from fairseq.tasks import TASK_REGISTRY
+from fairseq.tasks import register_task
+from fairseq.tasks.translation import TranslationTask
+
+
+@register_task('distillation')
+class DistillationTask(TranslationTask):
+    @staticmethod
+    def add_args(parser):
+        super().add_args(parser)
+        parser.add_argument('--ensemble-paths', help='Paths to ensemble models for distillation')
+        parser.add_argument('--ensemble-task', default='translation', choices=TASK_REGISTRY.keys(),
+                            help='Task used for training ensemble models')
+        # TODO ensemble-related args
+        pass
+
+    def __init__(self, args, src_dict, tgt_dict, models):
+        super().__init__(args, src_dict, tgt_dict)
+        self.ensemble = models
+
+    @classmethod
+    def setup_task(cls, args, **kwargs):
+        """Setup the task (e.g., load dictionaries).
+
+        Args:
+            args (argparse.Namespace): parsed command-line arguments
+        """
+        args.left_pad_source = options.eval_bool(args.left_pad_source)
+        args.left_pad_target = options.eval_bool(args.left_pad_target)
+
+        paths = args.data.split(os.pathsep)
+        assert len(paths) > 0
+        # find language pair automatically
+        if args.source_lang is None or args.target_lang is None:
+            args.source_lang, args.target_lang = data_utils.infer_language_pair(paths[0])
+        if args.source_lang is None or args.target_lang is None:
+            raise Exception('Could not infer language pair, please provide it explicitly')
+
+        # load dictionaries
+        src_dict = cls.load_dictionary(os.path.join(paths[0], 'dict.{}.txt'.format(args.source_lang)))
+        tgt_dict = cls.load_dictionary(os.path.join(paths[0], 'dict.{}.txt'.format(args.target_lang)))
+        assert src_dict.pad() == tgt_dict.pad()
+        assert src_dict.eos() == tgt_dict.eos()
+        assert src_dict.unk() == tgt_dict.unk()
+        print('| [{}] dictionary: {} types'.format(args.source_lang, len(src_dict)))
+        print('| [{}] dictionary: {} types'.format(args.target_lang, len(tgt_dict)))
+
+        # Load ensemble
+        print('| loading model(s) from {}'.format(args.ensemble_path))
+        models, _model_args = checkpoint_utils.load_model_ensemble(
+            args.ensemble_path.split(','),
+            task=args.ensemble_task,
+        )
+
+        # Optimize ensemble for generation (includes setting .eval())
+        for model in models:
+            model.make_generation_fast_(
+                beamable_mm_beam_size=None if args.no_beamable_mm else args.beam,
+                need_attn=False,
+            )
+            if args.fp16:
+                model.half()
+
+        return cls(args, src_dict, tgt_dict, models)
+
+    def train_step(self, sample, model, criterion, optimizer, ignore_grad=False):
+        """
+        Do forward and backward, and return the loss as computed by *criterion*
+        for the given *model* and *sample*.
+
+        Args:
+            sample (dict): the mini-batch. The format is defined by the
+                :class:`~fairseq.data.FairseqDataset`.
+            model (~fairseq.models.BaseFairseqModel): the model
+            criterion (~fairseq.criterions.FairseqCriterion): the criterion
+            optimizer (~fairseq.optim.FairseqOptimizer): the optimizer
+            ignore_grad (bool): multiply loss by 0 if this is set to True
+
+        Returns:
+            tuple:
+                - the loss
+                - the sample size, which is used as the denominator for the
+                  gradient
+                - logging outputs to display while training
+        """
+        model.train()
+
+        sample = self.compute_ensemble_logits(sample)
+
+        loss, sample_size, logging_output = criterion(model, sample)
+        if ignore_grad:
+            loss *= 0
+        optimizer.backward(loss)
+        return loss, sample_size, logging_output
+
+    def valid_step(self, sample, model, criterion):
+        model.eval()
+
+        sample = self.compute_ensemble_logits(sample)
+
+        with torch.no_grad():
+            loss, sample_size, logging_output = criterion(model, sample)
+        return loss, sample_size, logging_output
+
+    def inference_step(self, generator, models, sample, prefix_tokens=None):
+        # TODO what if model was trained with mixture of Dir()?
+        with torch.no_grad():
+            return generator.generate(models, sample, prefix_tokens=prefix_tokens)
+
+    @torch.no_grad()
+    def compute_ensemble_logits(self, sample):
+        use_cuda = torch.cuda.is_available() and not self.args.cpu
+
+        ensemble_logits = []
+
+        for model in self.ensemble:
+            if use_cuda:
+                model.cuda()
+            logits, _ = model(**sample['net_input'])
+            ensemble_logits.append(logits)
+            model.cpu()
+
+        sample['ensemble_logits'] = torch.stack(ensemble_logits, dim=2)
+        return sample
