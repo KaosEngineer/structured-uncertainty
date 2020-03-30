28d1afe050c9edb734de6673e86820d0a29a7406
Maksim Ryabinin <mryab@yandex-team.ru>
2020-03-30 20:30:57 +0300

    Scale by temp instead of temp**2

--- examples/ensemble_distribution_distillation/criterions/distillation.py
+++ examples/ensemble_distribution_distillation/criterions/distillation.py
@@ -73,8 +73,8 @@ class SequenceDistributionDistillationCritertion(FairseqCriterion):
         cost.masked_fill_(pad_mask, 0.)
 
         if reduce:
-            return torch.sum(cost) * temp ** 2 / num_classes
-        return cost * temp ** 2 / num_classes
+            return torch.sum(cost) * temp / num_classes
+        return cost * temp / num_classes
 
     @torch.no_grad()
     def compute_nll(self, model, net_output, sample, reduce):
