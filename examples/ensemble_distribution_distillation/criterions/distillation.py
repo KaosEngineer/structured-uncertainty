41457a2c146f80b5995479bc43f91cbbcf782d98
Maksim Ryabinin <mryab@yandex-team.ru>
2020-04-20 12:38:23 +0300

    Cleanup code, forward/backward KL losses with different aggregations

--- examples/ensemble_distribution_distillation/criterions/distillation.py
+++ examples/ensemble_distribution_distillation/criterions/distillation.py
@@ -6,12 +6,9 @@ from fairseq import utils
 from fairseq.criterions import FairseqCriterion, register_criterion
 
 
-@register_criterion('sequence_reverse_distillation')
-class ReverseKLCritertion(FairseqCriterion):
-
+class _DistillationCriterionBase(FairseqCriterion):
     def __init__(self, args, task):
         super().__init__(args, task)
-        self.smooth_val = 1e-8
 
     def forward(self, model, sample, reduce=True):
         # batch x len x n_tokens
@@ -32,23 +29,6 @@ class ReverseKLCritertion(FairseqCriterion):
             'sample_size': sample_size,
         }
         return loss, sample_size, logging_output
-
-    def compute_loss(self, model, net_output, ensemble_logits, sample, reduce):
-        logits = net_output[0]
-
-        probs = utils.softmax(logits, dim=-1)
-        teacher_probs = utils.softmax(ensemble_logits, dim=-1)
-        log_avg_teacher_probs = torch.log(teacher_probs.mean(2))
-
-        loss = -(probs * log_avg_teacher_probs).sum(-1)
-
-        # mask loss for padding tokens
-        pad_mask = model.get_targets(sample, net_output).eq(self.padding_idx)
-        loss.masked_fill_(pad_mask, 0.)
-
-        if reduce:
-            return torch.sum(loss)
-        return loss
 
     @torch.no_grad()
     def compute_nll(self, model, net_output, sample, reduce):
@@ -82,41 +62,57 @@ class ReverseKLCritertion(FairseqCriterion):
         }
 
 
-@register_criterion('sequence_forward_distillation')
-class ForwardKLCritertion(FairseqCriterion):
+@register_criterion('mean_reverse_kl_distillation')
+class ReverseKLCritertion(_DistillationCriterionBase):
+    def compute_loss(self, model, net_output, ensemble_logits, sample, reduce):
+        logits = net_output[0]
 
-    def __init__(self, args, task):
-        super().__init__(args, task)
-        self.smooth_val = 1e-8
+        probs = utils.softmax(logits, dim=-1)
+        teacher_log_probs = utils.log_softmax(ensemble_logits, dim=-1)
 
-    def forward(self, model, sample, reduce=True):
-        # batch x len x n_tokens
-        net_output = model(**sample['net_input'])
+        probs = probs.unsqueeze(2).expand_as(teacher_log_probs)
+        loss = torch.nn.functional.kl_div(teacher_log_probs, probs, reduction='none').mean(2).sum(-1)
 
-        # batch x len x ensemble_size x n_tokens
-        ensemble_logits = sample['ensemble_logits']
+        # mask loss for padding tokens
+        pad_mask = model.get_targets(sample, net_output).eq(self.padding_idx)
+        loss.masked_fill_(pad_mask, 0.)
 
-        loss = self.compute_loss(model, net_output, ensemble_logits, sample, reduce=reduce)
-        nll_loss = self.compute_nll(model, net_output, sample, reduce=reduce)
+        if reduce:
+            return torch.sum(loss)
+        return loss
 
-        sample_size = sample['target'].size(0) if self.args.sentence_avg else sample['ntokens']
-        logging_output = {
-            'loss': (utils.item(loss.data) if reduce else loss.data),
-            'nll_loss': utils.item(nll_loss.data) if reduce else nll_loss.data,
-            'ntokens': sample['ntokens'],
-            'nsentences': sample['target'].size(0),
-            'sample_size': sample_size,
-        }
-        return loss, sample_size, logging_output
+
+@register_criterion('reverse_kl_mean_distillation')
+class ReverseKLCritertion(_DistillationCriterionBase):
+    def compute_loss(self, model, net_output, ensemble_logits, sample, reduce):
+        logits = net_output[0]
+
+        probs = utils.softmax(logits, dim=-1)
+        avg_teacher_log_probs = utils.log_softmax(ensemble_logits, dim=-1).mean(2)
+
+        loss = torch.nn.functional.kl_div(avg_teacher_log_probs, probs, reduction='none').sum(-1)
+
+        # mask loss for padding tokens
+        pad_mask = model.get_targets(sample, net_output).eq(self.padding_idx)
+        loss.masked_fill_(pad_mask, 0.)
+
+        if reduce:
+            return torch.sum(loss)
+        return loss
+
+
+@register_criterion('mean_forward_kl_distillation')
+class ForwardKLCritertion(_DistillationCriterionBase):
 
     def compute_loss(self, model, net_output, ensemble_logits, sample, reduce):
         logits = net_output[0]
 
         log_probs = utils.log_softmax(logits, dim=-1)
         teacher_probs = utils.softmax(ensemble_logits, dim=-1)
-        avg_teacher_probs = teacher_probs.mean(2)
 
-        loss = -(avg_teacher_probs * log_probs).sum(-1)
+        # average loss over all teacher distributions
+        log_probs = log_probs.unsqueeze(2).expand_as(teacher_probs)
+        loss = torch.nn.functional.kl_div(log_probs, teacher_probs, reduction='none').mean(2).sum(-1)
 
         # mask loss for padding tokens
         pad_mask = model.get_targets(sample, net_output).eq(self.padding_idx)
@@ -126,66 +122,35 @@ class ForwardKLCritertion(FairseqCriterion):
             return torch.sum(loss)
         return loss
 
-    @torch.no_grad()
-    def compute_nll(self, model, net_output, sample, reduce):
-        lprobs = model.get_normalized_probs(net_output, log_probs=True)
-        lprobs = lprobs.view(-1, lprobs.size(-1))
-        target = model.get_targets(sample, net_output).view(-1, 1)
-        if target.dim() == lprobs.dim() - 1:
-            target = target.unsqueeze(-1)
-        nll_loss = -lprobs.gather(dim=-1, index=target)
-        pad_mask = target.eq(self.padding_idx)
-        if pad_mask.any():
-            nll_loss.masked_fill_(pad_mask, 0.)
-        else:
-            nll_loss = nll_loss.squeeze(-1)
-        if reduce:
-            nll_loss = nll_loss.sum()
-        return nll_loss
 
-    @staticmethod
-    def aggregate_logging_outputs(logging_outputs):
-        """Aggregate logging outputs from data parallel training."""
-        ntokens = sum(log.get('ntokens', 0) for log in logging_outputs)
-        nsentences = sum(log.get('nsentences', 0) for log in logging_outputs)
-        sample_size = sum(log.get('sample_size', 0) for log in logging_outputs)
-        return {
-            'loss': sum(log.get('loss', 0) for log in logging_outputs) / sample_size if sample_size > 0 else 0.,
-            'nll_loss': sum(log.get('nll_loss', 0) for log in logging_outputs) / ntokens / math.log(2) if ntokens > 0 else 0.,
-            'ntokens': ntokens,
-            'nsentences': nsentences,
-            'sample_size': sample_size,
-        }
+@register_criterion('forward_kl_mean_distillation')
+class ForwardKLCritertion(_DistillationCriterionBase):
+
+    def compute_loss(self, model, net_output, ensemble_logits, sample, reduce):
+        logits = net_output[0]
+
+        log_probs = utils.log_softmax(logits, dim=-1)
+        avg_teacher_probs = utils.softmax(ensemble_logits, dim=-1).mean(2)
+
+        loss = torch.nn.functional.kl_div(log_probs, avg_teacher_probs, reduction='none').sum(-1)
+
+        # mask loss for padding tokens
+        pad_mask = model.get_targets(sample, net_output).eq(self.padding_idx)
+        loss.masked_fill_(pad_mask, 0.)
+
+        if reduce:
+            return torch.sum(loss)
+        return loss
 
 
 @register_criterion('sequence_distribution_distillation')
-class SequenceDistributionDistillationCritertion(FairseqCriterion):
+class SequenceDistributionDistillationCritertion(_DistillationCriterionBase):
 
     def __init__(self, args, task):
         super().__init__(args, task)
         self.smooth_val = 1e-8
         self.tp_scaling = 1 - 1e-8
         self.temp = 1
-
-    def forward(self, model, sample, reduce=True):
-        # batch x len x n_tokens
-        net_output = model(**sample['net_input'])
-
-        # batch x len x ensemble_size x n_tokens
-        ensemble_logits = sample['ensemble_logits']
-
-        loss = self.compute_loss(model, net_output, ensemble_logits, sample, reduce=reduce, temp=self.temp)
-        nll_loss = self.compute_nll(model, net_output, sample, reduce=reduce)
-
-        sample_size = sample['target'].size(0) if self.args.sentence_avg else sample['ntokens']
-        logging_output = {
-            'loss': (utils.item(loss.data) if reduce else loss.data),
-            'nll_loss': utils.item(nll_loss.data) if reduce else nll_loss.data,
-            'ntokens': sample['ntokens'],
-            'nsentences': sample['target'].size(0),
-            'sample_size': sample_size,
-        }
-        return loss, sample_size, logging_output
 
     def compute_loss(self, model, net_output, ensemble_logits, sample, reduce, temp):
         # TODO add support for mixtures
@@ -222,34 +187,3 @@ class SequenceDistributionDistillationCritertion(FairseqCriterion):
         if reduce:
             return torch.sum(cost)
         return cost
-
-    @torch.no_grad()
-    def compute_nll(self, model, net_output, sample, reduce):
-        lprobs = model.get_normalized_probs(net_output, log_probs=True)
-        lprobs = lprobs.view(-1, lprobs.size(-1))
-        target = model.get_targets(sample, net_output).view(-1, 1)
-        if target.dim() == lprobs.dim() - 1:
-            target = target.unsqueeze(-1)
-        nll_loss = -lprobs.gather(dim=-1, index=target)
-        pad_mask = target.eq(self.padding_idx)
-        if pad_mask.any():
-            nll_loss.masked_fill_(pad_mask, 0.)
-        else:
-            nll_loss = nll_loss.squeeze(-1)
-        if reduce:
-            nll_loss = nll_loss.sum()
-        return nll_loss
-
-    @staticmethod
-    def aggregate_logging_outputs(logging_outputs):
-        """Aggregate logging outputs from data parallel training."""
-        ntokens = sum(log.get('ntokens', 0) for log in logging_outputs)
-        nsentences = sum(log.get('nsentences', 0) for log in logging_outputs)
-        sample_size = sum(log.get('sample_size', 0) for log in logging_outputs)
-        return {
-            'loss': sum(log.get('loss', 0) for log in logging_outputs) / sample_size if sample_size > 0 else 0.,
-            'nll_loss': sum(log.get('nll_loss', 0) for log in logging_outputs) / ntokens / math.log(2) if ntokens > 0 else 0.,
-            'ntokens': ntokens,
-            'nsentences': nsentences,
-            'sample_size': sample_size,
-        }
