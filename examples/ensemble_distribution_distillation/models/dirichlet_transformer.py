fcca2d35cba7a4d7f5d0dbe73af49ee8bb880113
Max Ryabinin <mryabinin0@gmail.com>
2020-06-30 01:31:43 +0300

    Remove architectures

--- examples/ensemble_distribution_distillation/models/dirichlet_transformer.py
+++ examples/ensemble_distribution_distillation/models/dirichlet_transformer.py
@@ -1,7 +1,7 @@
 import torch.nn as nn
 
 from fairseq.models import register_model, register_model_architecture
-from fairseq.models.transformer import TransformerDecoder, TransformerModel
+from fairseq.models.transformer import TransformerDecoder, TransformerModel, transformer_wmt_en_de_big
 
 
 @register_model('dirichlet_transformer')
@@ -20,7 +20,6 @@ class DirichletTransformerDecoder(TransformerDecoder):
     def __init__(self, args, dictionary, embed_tokens, no_encoder_attn=False):
         super().__init__(args, dictionary, embed_tokens, no_encoder_attn)
         self.dirichlet_projection = nn.Linear(self.output_embed_dim, 1)
-        # nn.init.normal_(self.dirichlet_projection.weight, mean=0, std=self.output_embed_dim ** -0.5)
 
     def extract_features(
             self,
@@ -45,54 +44,6 @@ class DirichletTransformerDecoder(TransformerDecoder):
         return x, extra
 
 
-@register_model_architecture('dirichlet_transformer', 'dirichlet_transformer')
-def base_architecture(args):
-    args.encoder_embed_path = getattr(args, 'encoder_embed_path', None)
-    args.encoder_embed_dim = getattr(args, 'encoder_embed_dim', 512)
-    args.encoder_ffn_embed_dim = getattr(args, 'encoder_ffn_embed_dim', 2048)
-    args.encoder_layers = getattr(args, 'encoder_layers', 6)
-    args.encoder_attention_heads = getattr(args, 'encoder_attention_heads', 8)
-    args.encoder_normalize_before = getattr(args, 'encoder_normalize_before', False)
-    args.encoder_learned_pos = getattr(args, 'encoder_learned_pos', False)
-    args.decoder_embed_path = getattr(args, 'decoder_embed_path', None)
-    args.decoder_embed_dim = getattr(args, 'decoder_embed_dim', args.encoder_embed_dim)
-    args.decoder_ffn_embed_dim = getattr(args, 'decoder_ffn_embed_dim', args.encoder_ffn_embed_dim)
-    args.decoder_layers = getattr(args, 'decoder_layers', 6)
-    args.decoder_attention_heads = getattr(args, 'decoder_attention_heads', 8)
-    args.decoder_normalize_before = getattr(args, 'decoder_normalize_before', False)
-    args.decoder_learned_pos = getattr(args, 'decoder_learned_pos', False)
-    args.attention_dropout = getattr(args, 'attention_dropout', 0.)
-    args.activation_dropout = getattr(args, 'activation_dropout', 0.)
-    args.activation_fn = getattr(args, 'activation_fn', 'relu')
-    args.dropout = getattr(args, 'dropout', 0.1)
-    args.adaptive_softmax_cutoff = getattr(args, 'adaptive_softmax_cutoff', None)
-    args.adaptive_softmax_dropout = getattr(args, 'adaptive_softmax_dropout', 0)
-    args.share_decoder_input_output_embed = getattr(args, 'share_decoder_input_output_embed', False)
-    args.share_all_embeddings = getattr(args, 'share_all_embeddings', False)
-    args.no_token_positional_embeddings = getattr(args, 'no_token_positional_embeddings', False)
-    args.adaptive_input = getattr(args, 'adaptive_input', False)
-    args.no_cross_attention = getattr(args, 'no_cross_attention', False)
-    args.cross_self_attention = getattr(args, 'cross_self_attention', False)
-    args.layer_wise_attention = getattr(args, 'layer_wise_attention', False)
-
-    args.decoder_output_dim = getattr(args, 'decoder_output_dim', args.decoder_embed_dim)
-    args.decoder_input_dim = getattr(args, 'decoder_input_dim', args.decoder_embed_dim)
-
-    args.no_scale_embedding = getattr(args, 'no_scale_embedding', False)
-    args.layernorm_embedding = getattr(args, 'layernorm_embedding', False)
-
-
 @register_model_architecture('dirichlet_transformer', 'dirichlet_transformer_wmt_en_de_big')
-def transformer_wmt_en_de_big(args):
-    # transformer_vaswani_wmt_en_de_big
-    args.encoder_embed_dim = getattr(args, 'encoder_embed_dim', 1024)
-    args.encoder_ffn_embed_dim = getattr(args, 'encoder_ffn_embed_dim', 4096)
-    args.encoder_attention_heads = getattr(args, 'encoder_attention_heads', 16)
-    args.encoder_normalize_before = getattr(args, 'encoder_normalize_before', False)
-    args.decoder_embed_dim = getattr(args, 'decoder_embed_dim', 1024)
-    args.decoder_ffn_embed_dim = getattr(args, 'decoder_ffn_embed_dim', 4096)
-    args.decoder_attention_heads = getattr(args, 'decoder_attention_heads', 16)
-    args.dropout = getattr(args, 'dropout', 0.3)
-
-    args.attention_dropout = getattr(args, 'attention_dropout', 0.1)
-    base_architecture(args)
+def dirichlet_transformer_wmt_en_de_big(args):
+    transformer_wmt_en_de_big(args)
