Bug fixes, compute ensemble precision from top-k probabilities

examples/ensemble_distribution_distillation/criterions/distillation.py
@@ -45,14 +45,33 @@ def compute_mkl(ensemble_probs, ensemble_mean_probs, ensemble_logprobs):
 
 
 @torch.no_grad()
-def compute_ensemble_stats(sample):
+def compute_ensemble_stats(sample, precision_from_topk):
     ensemble_logits = sample['ensemble_logits']
     ensemble_probs = utils.softmax(ensemble_logits, dim=-1)
     ensemble_mean_probs = ensemble_probs.mean(dim=2)
     ensemble_logprobs = utils.log_softmax(ensemble_logits, dim=-1)
 
-    epkl = compute_epkl(ensemble_probs, ensemble_mean_probs, ensemble_logprobs)
-    mkl = compute_mkl(ensemble_probs, ensemble_mean_probs, ensemble_logprobs)
+    if precision_from_topk != -1:
+        sorted_avg_probs, argsort_inds = ensemble_mean_probs.sort(dim=-1, descending=True)
+        # get indices of k most likely classes
+        highest_probs_inds = argsort_inds[..., :precision_from_topk]
+        lowest_probs_inds = argsort_inds[..., precision_from_topk:]
+
+        sizes = (-1, -1, ensemble_probs.size(2), -1)
+
+        # take probabilities for classes in top-k, sum for other classes
+        probs_in_topk = ensemble_probs.gather(3, highest_probs_inds.unsqueeze(2).expand(*sizes))
+        probs_not_in_topk = ensemble_probs.gather(3, lowest_probs_inds.unsqueeze(2).expand(*sizes)).sum(3, keepdim=True)
+
+        ensemble_probs_aggregated = torch.cat((probs_in_topk, probs_not_in_topk), dim=3)
+        ensemble_logprobs_aggregated = ensemble_probs_aggregated.log()
+        ensemble_mean_probs_aggregated = ensemble_probs_aggregated.mean(dim=2)
+
+        epkl = compute_epkl(ensemble_probs_aggregated, ensemble_mean_probs_aggregated, ensemble_logprobs_aggregated)
+        mkl = compute_mkl(ensemble_probs_aggregated, ensemble_mean_probs_aggregated, ensemble_logprobs_aggregated)
+    else:
+        epkl = compute_epkl(ensemble_probs, ensemble_mean_probs, ensemble_logprobs)
+        mkl = compute_mkl(ensemble_probs, ensemble_mean_probs, ensemble_logprobs)
 
     stats = {
         'probs': ensemble_probs,
@@ -80,6 +99,7 @@ class _DistillationCriterionBase(FairseqCriterion):
         self.label_smoothing = args.label_smoothing
         self.task = task
         self.xent_type = args.xent_type
+        self.precision_from_topk = args.precision_from_topk
 
     @staticmethod
     def add_args(parser):
@@ -87,6 +107,7 @@ class _DistillationCriterionBase(FairseqCriterion):
         parser.add_argument('--label-smoothing', default=0., type=float, metavar='D',
                             help='epsilon for label smoothing, 0 means no label smoothing')
         parser.add_argument('--xent-type', choices=('xent', 'forward_kl'), default='xent')
+        parser.add_argument('--precision-from-topk', type=int, default=-1)
 
     def forward(self, model, sample, reduce=True):
         xent_weight = self.task.xent_weight
@@ -94,7 +115,7 @@ class _DistillationCriterionBase(FairseqCriterion):
         # batch x len x n_tokens
         net_output = model(**sample['net_input'])
 
-        ensemble_stats = compute_ensemble_stats(sample)
+        ensemble_stats = compute_ensemble_stats(sample, self.precision_from_topk)
 
         loss, stats = self.compute_loss(model, net_output, ensemble_stats, sample, reduce=reduce)
 

examples/ensemble_distribution_distillation/tasks/distillation.py
@@ -327,14 +327,14 @@ class DistillationTask(TranslationTask):
         model = models.build_model(args, self)
         if args.init_from_model is not None:
             if self.ensemble:
-                state_dict = self.ensemble[args.init_from_model].state_dict()
+                state_dict = self.ensemble[self.init_from_model].state_dict()
                 if not model.decoder.share_input_output_embed:
                     state_dict['decoder.embed_out'] = state_dict['decoder.embed_tokens.weight']
                 model.load_state_dict(state_dict, strict=False)
             else:
                 print('Warning: ensemble_paths was empty and init_from_model is not None. Parameters were not overwritten')
 
-        if args.freeze_weights_until is not None and args.freeze_weights_until > 0:
+        if self.freeze_weights_until is not None and self.freeze_weights_until > 0:
             freeze_module_params(model.encoder)
             freeze_module_params(model.decoder)
             if model.decoder.share_input_output_embed:
@@ -342,8 +342,11 @@ class DistillationTask(TranslationTask):
             else:
                 model.decoder.embed_out.requires_grad = True
 
-        if (args.parametrization != 'exp' or args.model_offset != 0) and not 'dirichlet' in args.arch:
+        if (self.parametrization != 'exp' or args.model_offset != 0) and not 'dirichlet' in args.arch:
             print('Patching get_normalized_probs')
+
+            parametrization_func = self.parametrization_func
+            model_offset = args.model_offset
 
             # patching get_normalized_probs, as we may use something other than exp for mapping logits to positive numbers
             def patched_get_normalized_probs(self, net_output, log_probs, sample=None):
@@ -353,7 +356,7 @@ class DistillationTask(TranslationTask):
                     raise NotImplementedError()
 
                 logits = net_output[0]
-                unnormalized_probs = prob_parametrization[args.parametrization](logits.float()) + args.model_offset
+                unnormalized_probs = parametrization_func(logits.float()) + model_offset
                 probs = unnormalized_probs / unnormalized_probs.sum(dim=-1, keepdim=True)
                 # add small constants for numerical stability
                 probs = probs * (1 - 1e-8) + 1e-8 * (1 / probs.size(-1))

fairseq/uncertainty.py
@@ -96,17 +96,13 @@ def token_aep_uncertainty(pos_enscores):
     return expr_pos_scores, eoe_ub, expr_pos_mkl, prex_pos_scores, prex_pos_mkl
 
 
-@torch.jit.script
-def eps():
-    return 1e-8
+EPS = 1e-8
 
 
-@torch.jit.script
 def entropy(probs, dim: int = -1):
-    return -(probs * (probs + eps()).log()).sum(dim=dim)
+    return -(probs * (probs + EPS).log()).sum(dim=dim)
 
 
-@torch.jit.script
 def compute_token_dirichlet_uncertainties(dirichlet_params, concentrations, expected_dirichlet):
     batch_size, num_tokens, vocab_size = dirichlet_params.size()
 
@@ -119,16 +115,13 @@ def compute_token_dirichlet_uncertainties(dirichlet_params, concentrations, expe
     assert (mutual_information >= 0).all()
     epkl = (vocab_size - 1) / concentrations.squeeze(2)
     assert (epkl >= 0).all()
-    mkl = (-expected_dirichlet * (torch.digamma(dirichlet_params) - torch.digamma(concentrations))).sum(dim=-1) - entropy_of_expected
+    mkl = (-expected_dirichlet * (torch.digamma(dirichlet_params + EPS) - torch.digamma(concentrations + EPS))).sum(
+        dim=-1) - entropy_of_expected
     assert (mkl >= 0).all()
-    RTOL: float = 0.0
-    ATOL: float = 1.0
-    assert torch.allclose(epkl, mutual_information + mkl, rtol=RTOL, atol=ATOL)
 
     return entropy_of_expected, expected_entropy, mutual_information, epkl, mkl
 
 
-@torch.jit.script
 def compute_sequence_dirichlet_uncertainties(dirichlet_params, concentrations, log_expected_probs, predict_inds, mask, num_tokens):
     unsqueezed_inds = predict_inds.unsqueeze(-1)
 
@@ -140,7 +133,7 @@ def compute_sequence_dirichlet_uncertainties(dirichlet_params, concentrations, l
     scores = -log_probs / num_tokens
     # scores >=0
 
-    token_scores_mkl = (torch.digamma(concentrations) - torch.digamma(dirichlet_params.gather(-1, unsqueezed_inds))
+    token_scores_mkl = (torch.digamma(concentrations + EPS) - torch.digamma(dirichlet_params.gather(-1, unsqueezed_inds) + EPS)
                         ).squeeze(2) + token_log_probs
 
     if mask.any():