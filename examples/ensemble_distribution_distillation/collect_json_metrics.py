f218036010d5cd493efbb6746f8bd8425fc91eaf
Max Ryabinin <mryabinin0@gmail.com>
2020-09-24 10:29:01 +0300

    Add sequence-level Spearman metrics wrt ensemble

--- examples/ensemble_distribution_distillation/collect_json_metrics.py
+++ examples/ensemble_distribution_distillation/collect_json_metrics.py
@@ -1,8 +1,11 @@
 import json
-from pathlib import Path
-from argparse import ArgumentParser
+import os
 import re
+from argparse import ArgumentParser
+from pathlib import Path
+
 import numpy as np
+import scipy.stats
 
 
 def parse_uncertainty_metrics(f, report_nbest=None):
@@ -21,7 +24,7 @@ def parse_uncertainty_metrics(f, report_nbest=None):
     return result
 
 
-def main(decode_path, reference_path, output_path):
+def main(decode_path, reference_path, output_path, ensemble_decode_path):
     with open(decode_path / 'results-test.txt') as test_preds_f:
         result_line = test_preds_f.read()
         test_bleu = float(re.search(r"BLEU = (\d*.\d*)", result_line).group(1))
@@ -50,11 +53,30 @@ def main(decode_path, reference_path, output_path):
             **{f'seq_error_{testset}_{key}': value for key, value in decode_mc_seq.items()},
         })
 
+    sequence_spearman_metrics = dict()
+    if ensemble_decode_path is not None:
+        for testset in ('test', 'test12', 'test14'):
+            for metric in ('epkl', 'mkl', 'score', 'entropy_expected', 'score_npmi'):
+                try:
+                    uncertainties_for_model = np.loadtxt(decode_path / testset / f'{metric}.txt')
+                    uncertainties_for_ensemble = np.loadtxt(ensemble_decode_path / testset / f'{metric}.txt')
+                except Exception as e:
+                    if os.path.exists(ensemble_decode_path / testset):
+                        print(os.listdir(ensemble_decode_path / testset))
+                    if os.path.exists(decode_path / testset):
+                        print(os.listdir(decode_path / testset))
+                    print(testset, metric, e)
+                    continue
+                metric_name = metric if metric != 'score_npmi' else 'smkl'
+                spearman_correlation = scipy.stats.spearmanr(uncertainties_for_model, uncertainties_for_ensemble).correlation
+                sequence_spearman_metrics[f'spearman_{testset}_{metric_name}'] = spearman_correlation
+
     result_dict = {
         'test BLEU': test_bleu,
         'test NLL': scores.mean(),
         **ood_metrics,
-        **seq_error_metrics
+        **seq_error_metrics,
+        **sequence_spearman_metrics,
     }
     with open(output_path, 'w+') as output_f:
         json.dump(result_dict, output_f, indent=2, sort_keys=True)
@@ -66,5 +88,6 @@ if __name__ == '__main__':
     parser.add_argument('-d', '--decode-path', required=True, type=Path)
     parser.add_argument('-r', '--reference-path', required=True, type=Path)
     parser.add_argument('-o', '--output-path', required=True, type=Path)
+    parser.add_argument('-e', '--ensemble-decode-path', type=Path)
     args = parser.parse_args()
-    main(args.decode_path, args.reference_path, args.output_path)
+    main(args.decode_path, args.reference_path, args.output_path, args.ensemble_decode_path)
